5ff0e5b42dcebf2d4cd82411a4f350966f56b4b7
dookdic <dookdic@gmail.com>
2020-02-07 13:40:16 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'OW9lbngKJs5NYK9fdHfBRskNGcNfpAgo/ajcfuT/3kQEi+eORymO2i8BnVf+foI5ZNU9Usp5aFyM9JwqPMVLcTXjclnReEJ1eck1TkOz7OWWjtSVIJ/0qeaZLS0Qov4HyOJGHqc4hM1zVrkIVnoRzwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
